12325fa61946f3063da1272b0358cf53841bca87
치훈 <rlaclgns321@naver.com>
2025-11-23 15:16:32 +0900

    chore: msw 셋팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
-import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
-
+import { StrictMode } from 'react'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    // true MSW on, false MSW off
+    return
+  }
+  const { worker } = await import('./mocks/browser') // 이전에 설정한 브라우저 환경설정 import
+
+  return worker.start({
+    onUnhandledRequest: 'bypass', // 모킹되지 않은 요청은 실제 서버로 전달
+  })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+})
