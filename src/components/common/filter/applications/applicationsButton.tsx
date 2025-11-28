e51a7fc1bd1908e3e01b79d0886f1881c05d6421
치훈 <rlaclgns321@naver.com>
2025-11-28 15:06:05 +0900

    refactor: 공통 컴포넌트 SearchInput·FilterSelect에 className 및 twMerge 적용

--- src/components/common/filter/applications/applicationsButton.tsx
+++ src/components/common/filter/applications/applicationsButton.tsx
@@ -1,8 +1,15 @@
-export default function ApplicationsButton() {
+type Props = {
+  setOpen: (value: boolean) => void
+}
+
+export default function ApplicationsButton({ setOpen }: Props) {
   return (
     <div className="flex h-[73px] w-full items-center justify-end px-6 py-4">
       <div className="flex h-10 w-auto items-center">
-        <div className="mr-3 cursor-pointer rounded-lg bg-[#6B7280] px-4 py-2 text-white hover:bg-[#585e6a] active:font-bold">
+        <div
+          onClick={() => setOpen(false)}
+          className="mr-3 cursor-pointer rounded-lg bg-[#6B7280] px-4 py-2 text-white hover:bg-[#585e6a] active:font-bold"
+        >
           취소
         </div>
         <div className="cursor-pointer rounded-lg bg-[#EAB308] px-4 py-2 text-white hover:bg-[#854D0E] active:font-bold">
--- src/pages/recruitment/applications/ToggleSelectedTag.tsx
+++ src/pages/recruitment/applications/ToggleSelectedTag.tsx
@@ -1,15 +1,23 @@
+import clsx from 'clsx'
+import { ListFilter } from 'lucide-react'
 import { useEffect, useState } from 'react'
 
-import { FilterBar } from '@/components/common/filter'
+import { twMerge } from 'tailwind-merge'
+
+import { FilterSelect, SearchInput } from '@/components/common/filter'
 import ApplicationsButton from '@/components/common/filter/applications/applicationsButton'
 import ApplicationSearch from '@/components/common/filter/applications/applicationSearch'
 import GetSelectedTags from '@/components/common/filter/applications/getSelectedTags'
 import ToggleSelectedTag from '@/components/common/filter/applications/toggleSelectedTag'
 import Modal from '@/components/common/Modal'
 
+const LABEL_STYLE = 'text-sm text-[#374151]'
+const BOX_STYLE = 'w-[256px] h-9 focus:ring-0 focus:border-0'
+
 export default function ApplicationManagementPage() {
   const [search, setSearch] = useState('')
   const [status, setStatus] = useState('')
+  const [open, setOpen] = useState(false)
   const [role, setRole] = useState('')
   const [queryParams, setQueryParams] = useState({
     page: 1,
@@ -27,51 +35,65 @@ export default function ApplicationManagementPage() {
   return (
     <div className="space-y-4 rounded-lg bg-white p-6">
       <Modal
-        isOpen
-        onClose={() => {}}
+        isOpen={open}
         title="태그 필터 선택"
         contentClassName="p-0"
-        footer={<ApplicationsButton />}
+        footer={<ApplicationsButton setOpen={setOpen} />}
         footerClassName="p-0"
         topCloseButton
+        onClose={() => setOpen(false)}
       >
         <ApplicationSearch />
         <GetSelectedTags />
         <ToggleSelectedTag />
       </Modal>
-      {`queryParams: ${JSON.stringify(queryParams)}`}
-      <FilterBar
-        searchConfig={{
-          // SearchInput의 label 기본값은 검색
-          placeholder: '이메일, 닉네임, 이름, ID 검색...',
-          value: search,
-          onChange: setSearch,
-        }}
-        filters={[
-          {
-            label: '공고 상태',
-            options: [
-              { label: '활성', value: 'active' },
-              { label: '비활성', value: 'inactive' },
-              { label: '탈퇴요청', value: 'withdraw' },
-            ],
-            value: status,
-            onChange: setStatus,
-            placeholder: '상태',
-          },
-          {
-            label: '태그 필터',
-            options: [
-              { label: '관리자', value: 'admin' },
-              { label: '스태프', value: 'staff' },
-              { label: '일반회원', value: 'user' },
-            ],
-            value: role,
-            onChange: setRole,
-            placeholder: '권한',
-          },
-        ]}
-      />
+
+      <div className="flex items-center">
+        <SearchInput
+          className="mr-4"
+          labelClassName={LABEL_STYLE}
+          searchClassName={BOX_STYLE}
+          inputClassName="outline-0 focus:ring-0 focus:border-gray-300"
+          placeholder="공고명 검색"
+          value={search}
+          onChange={setSearch}
+        />
+        <FilterSelect
+          className="mr-4"
+          key={status}
+          label="공고 상태"
+          labelClassName={LABEL_STYLE}
+          selectClassName={BOX_STYLE}
+          options={[
+            { label: '모집중', value: 'open' },
+            { label: '마감', value: 'close' },
+          ]}
+          value={status}
+          onChange={setStatus}
+          placeholder="전체"
+        />
+        <div className="flex flex-col">
+          <div
+            className={twMerge(
+              clsx('mb-2 text-sm font-medium text-[##374151]')
+            )}
+          >
+            태그 필터
+          </div>
+          <div
+            onClick={() => setOpen(true)}
+            className={twMerge(
+              clsx(
+                'flex cursor-pointer items-center justify-between rounded-lg border border-[#D1D5DB] px-3 py-2',
+                BOX_STYLE
+              )
+            )}
+          >
+            <div className="text-sm text-[#374151]">태그 입력</div>
+            <ListFilter className="w-4 text-[#9CA3AF]" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
