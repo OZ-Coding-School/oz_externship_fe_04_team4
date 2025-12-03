de2b5d959b447a7d1157b54896d66741aa543d98
치훈 <rlaclgns321@naver.com>
2025-12-03 14:08:55 +0900

    feat: 컴포넌트 SearchInput 대신 엔터 기반 구인공고 검색 인풋 로직 구현

--- src/features/recruitment/ui/RecruitmentFilter/index.tsx
+++ src/features/recruitment/ui/RecruitmentFilter/index.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx'
-import { ListFilter } from 'lucide-react'
+import { ListFilter, Search } from 'lucide-react'
 
+import { useState, type KeyboardEvent } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-import { FilterSelect, SearchInput } from '@/components/common/filter'
+import { FilterSelect } from '@/components/common/filter'
 import TagFilterPreview from '@/components/common/tag/TagFilterPreview'
 import { useRecruitmentModalStore } from '@/store/recruitment/useRecruitmentModalStore'
 import { useRecruitmentSearchStore } from '@/store/recruitment/useRecruitmentSearchStore'
@@ -16,21 +17,37 @@ const BOX_STYLE = 'w-[256px] h-9 focus:ring-0 focus:border-0'
 export default function RecruitmentFilter() {
   const { openModal } = useRecruitmentModalStore()
 
-  const { keyword, setKeyword } = useRecruitmentSearchStore()
+  const [search, setSearch] = useState('')
+  const { setKeyword } = useRecruitmentSearchStore()
   const { status, setStatus } = ueeRecruitmentStatusStore()
   const { selectedTagsResult } = useRecruitmentTagListStore()
 
+  const handleSearchSubmit = () => {
+    setKeyword(search.trim())
+  }
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      handleSearchSubmit()
+    }
+  }
+
   return (
     <>
-      <SearchInput
-        className="mr-4"
-        labelClassName={LABEL_STYLE}
-        searchClassName={BOX_STYLE}
-        inputClassName="outline-0 focus:ring-0 focus:border-gray-300"
-        placeholder="공고명 검색"
-        value={keyword}
-        onChange={setKeyword}
-      />
+      <div className="mr-4 flex flex-col">
+        <div className={'mb-2 font-medium text-[##374151]'}>검색</div>
+        <div className="flex w-[256px] items-center rounded-lg border border-[#D1D5DB] px-3 py-2">
+          <Search className="mr-3 w-4 text-gray-400" />
+          <input
+            type="text"
+            placeholder="공고명 검색 후 엔터"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            onKeyDown={handleKeyDown}
+            className="h-5 text-sm outline-0"
+          />
+        </div>
+      </div>
 
       <FilterSelect
         className="mr-4"
@@ -47,7 +64,7 @@ export default function RecruitmentFilter() {
         placeholder="전체"
       />
 
-      <div className="mr-4 flex flex-col">
+      <div className="flex flex-col">
         <div className={'mb-2 text-sm font-medium text-[##374151]'}>
           태그 필터
         </div>
